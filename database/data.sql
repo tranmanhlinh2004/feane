1ff6fb62cba0cd543ceb54483e8ee8992e8d43ad
tranmanhlinh <tranmanhlinh204@gmail.com>
2025-11-12 17:40:31 +0700

    upload repo

---
+++ database/data.sql
@@ -0,0 +1,7 @@
+﻿insert into ParentMenu values
+(N'Trang chủ', 'feane/trangchu', 1, 1, 1),
+(N'Thực đơn', 'feane/thucdon', 1, 2, 1),
+(N'Giới thiệu', 'feane/gioithieu', 1, 3, 1),
+(N'Blog', 'feane/blog', 1, 4, 1),
+(N'Chính sách', 'feane/chinhsach', 1, 5, 1);
+select * from ParentMenu
